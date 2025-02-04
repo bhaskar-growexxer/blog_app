e6ef43d51aab5402239f71194366aa67e21dfffb
Bhaskar <bhaskar.reddy@growexx.com>
2025-02-04 13:14:52 +0530

    added sonar into project and fixed some issues

--- api/config/cors.php
+++ api/config/cors.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Cross-Origin Resource Sharing (CORS) Configuration
@@ -14,21 +13,12 @@ return [
     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     |
     */
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE'], // Limit methods for security
+    'allowed_origins' => ['http://localhost:4200'], // Only allow requests from localhost:4200
     'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
+    'allowed_headers' => ['Content-Type', 'Authorization'], // Specify allowed headers
     'exposed_headers' => [],
-
     'max_age' => 0,
-
-    'supports_credentials' => false,
-
+    'supports_credentials' => false, // Set to true if you need to allow cookies or HTTP authentication
 ];
